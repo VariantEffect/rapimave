eb1fca12041660dfc2ee2c2a83f07e03d52115d0
Jochen Weile <jochenweile@gmail.com>
2018-12-17 15:37:26 -0500

    license information added

--- tests/testthat/testRapimave.R
+++ tests/testthat/testRapimave.R
@@ -1,3 +1,20 @@
+# Copyright (C) 2018  Jochen Weile, Roth Lab
+#
+# This file is part of rapimave.
+#
+# rapimave is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# rapimave is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with rapimave.  If not, see <https://www.gnu.org/licenses/>.
+
 library(rapimave)
 
 context("MaveDB API access")
